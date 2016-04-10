2e649326cd7ffa8492ba7329a3b05efee7b708ca
sadrusaleem <sadruddinsaleem17@gmail.com>
2016-04-10 15:56:05 +0000

    demo complete

--- bern_sms.py
+++ bern_sms.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
 import os
 from cleverbot import Cleverbot
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 known_demos = {'black', 'young'}
 
@@ -16,6 +18,8 @@ def respond():
     resp = twilio.twiml.Response()
     
     body = request.values.get('Body', None)
+    print request.values
+    print body
     
     demos_mentioned = []
     
@@ -23,16 +27,31 @@ def respond():
         if demo in body.lower():
             demos_mentioned.append(demo)
     
+    get_feedback = False
     if demos_mentioned:
         response = get_response_for_demos(demos_mentioned)
         #want to find out if what we gave was effective
         get_feedback = True
     else:
-        response = cb.ask(body)
+        #no demos were detected so no feedback should be gathered
+        get_feedback = False
+        
+        #they might be giving us feedback from the previous convo
+        feedback = handle_feedback(body, resp)
+        
+        if feedback:
+            response = feedback
+        else:
+            #nope they're not, let's just use cleverbot to reply
+            response = cb.ask(body)
     
-    resp.message(response)
-    
-    #TODO: get feedback
+    if get_feedback:
+        store_cookies(demos_mentioned)
+        response += "*****\nWas this information helpful? (Y/N)"
+        
+    resp.sms(response)
+
+    #TODO: store user info (maybe?)
         
     return str(resp)
 
@@ -45,6 +64,31 @@ def get_response_for_demos(demos_mentioned):
         response = response + PREFACE_TEXT.format(demo) + demo_to_response.get(demo) + "\n\n*****\n\n"
     
     return response
+
+def store_cookies(demos):
+    print 'storing ' + str(demos)
+    session['demos'] = demos
+    
+def handle_feedback(body, sms_sender):
+    '''
+    handles the possible case where a user is giving us feedback from a previous
+    conversation. only goes to this method if we didn't find an identifier
+    
+    returns a thank you string if there was feedback else None
+    '''
+    body_words = body.lower().split()
+    YES = ['yes', 'y']
+    NO = ['no', 'n']
+    
+    if any(x in YES + NO for x in body_words):
+        #check if we even have talked to this guy before
+        if session.get('demos'):
+            #TODO:tell api to strengthen previous connections
+            print 'strengthening connections :' + str(session['demos'])
+            return "Thank you for your feedback! Your answers help make WhyIBern even stronger!"
+    
+    return None
+    
     
 if __name__ == "__main__":
     app.run(debug=True, 
